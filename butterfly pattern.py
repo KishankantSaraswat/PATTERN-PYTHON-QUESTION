afd9515fe3b7d717615a0c8a9f3f320872488d41
KishankantSaraswat <122577473+KishankantSaraswat@users.noreply.github.com>
2023-04-02 06:47:00 -0700

    Add files via upload

---
+++ butterfly pattern.py
@@ -0,0 +1,10 @@
+num=int(input("enter the number of rows")
+for i in range(1,n+1):
+  print("* "*i)
+for i in range(num-1,0,-1)
+ print("* "*i)
+num=int(input("enter the number of rows")
+for i in range(1,n+1):
+  print(" "*(num-i)+"* "*i)
+for i in range(num-1,0,-1)
+ print(" "*(num-i)+"* "*i)
